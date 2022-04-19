9bec1cd02997801f43b7a94996a20daf75d16cbe
Binbin Qian <qianbinbin@hotmail.com>
2022-04-19 20:31:38 +0800

    Create shell script

---
+++ block.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+HOST="localhost:9091"
+
+AUTH="username:password"
+
+CLIENTS="xunlei thunder gt0002 xl0012 xfplay dandanplay dl3760 qq"
+
+LIST="$HOME/.config/transmission-daemon/blocklists/leechers.txt"
+BIN="$LIST.bin"
+
+# Clear blocklist
+# 0=disable
+# TIMEOUT_SECONDS=$((60 * 60 * 24)) # 24 hours
+TIMEOUT_SECONDS=0
+
+error() { echo "$@" >&2; }
+
+pattern=$(echo "$CLIENTS" | xargs | sed 's/ /\\)\\|\\(/g')
+pattern="\(\($pattern\)\)"
+
+start=$(date +%s)
+while true; do
+  diff=$(($(date +%s) - start))
+  if [ $TIMEOUT_SECONDS -ne 0 ] && [ $diff -ge $TIMEOUT_SECONDS ]; then
+    echo "Clearing blocklist"
+    rm -f "$LIST"
+    rm -f "$BIN"
+    start=$(date +%s)
+  fi
+
+  peers=$(transmission-remote "$HOST" --auth "$AUTH" --torrent all --info-peers)
+  leechers=$(echo "$peers" | grep -i "$pattern")
+  echo "$leechers" | while read -r leecher; do
+    [ -z "$leecher" ] && continue
+    # https://en.wikipedia.org/wiki/PeerGuardian#P2P_plaintext_format
+    client=$(echo "$leecher" | grep -i "$pattern.*$" -o)
+    ip=$(echo "$leecher" | awk '{ print $1 }')
+    line="$client:$ip-$ip"
+    grep -qs "$line" "$LIST" && continue
+    echo "Blocking: $line"
+    echo "$line" >>"$LIST"
+  done
+
+  # reload: https://github.com/transmission/transmission/blob/main/docs/Editing-Configuration-Files.md#reload-settings
+  # transmission-remote "$HOST" --auth "$AUTH" --blocklist-update
+  killall -HUP transmission-daemon
+  sleep 30
+done
